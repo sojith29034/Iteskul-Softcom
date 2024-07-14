9b314ffa2577ee0e51bf35101a8e226408b5aeec
Sojith Sunny <sojithsunny29034@gmail.com>
2024-07-14 22:48:05 +0530

    Update index.py

--- StudentData/index.py
+++ StudentData/index.py
@@ -39,9 +39,10 @@ def run_main_app():
     
         for file in files:
             url = base_url + file
+            st.write(url)
             response = requests.get(url)
             if response.status_code == 200:
-                uploaded_files.append((file, BytesIO(response.content)))
+                uploaded_files.append((BytesIO(response.content)))
             else:
                 st.warning(f"Failed to fetch {file} from GitHub.")
     
